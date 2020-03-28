a903418011fa47c0ff1f03c21f9b0fd675073420
Whale <mitr1659@edu.kadk.dk>
2020-03-28 04:25:43 +0100

    Simple shader

---
+++ Assets/Scripts/Michael/MaterialScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialScript : MonoBehaviour
+{
+    Transform target;
+    GameManager manager;
+    [Range(0, 1)]
+    public float dissolveValue;
+    Renderer matRend;
+    Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameManager>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        matRend = GetComponent<Renderer>();
+        mat = matRend.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((target.position - transform.position).magnitude < manager.minimumDissolveRange)
+        {
+            dissolveValue = 0;
+        }
+        else if ((target.position - transform.position).magnitude > manager.maximumDissolveRange)
+        {
+            dissolveValue = 1;
+        }
+        else {
+            dissolveValue = Mathf.Abs((manager.maximumDissolveRange - manager.minimumDissolveRange) - (target.position - transform.position).magnitude - manager.minimumDissolveRange) / (manager.maximumDissolveRange - manager.minimumDissolveRange);
+        }
+
+            mat.SetFloat("_DissolveValue", dissolveValue);
+    }
+}
