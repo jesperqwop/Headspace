141625da2e70d3c22852f49d2e3c1ef59ecff24d
jesperqwop <jespertuan@hotmail.com>
2020-04-01 20:31:32 +0200

    whiteboxed up to stage 3

---
+++ Assets/Scripts/Jesper scripts/Keytracker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keytracker : MonoBehaviour
+{
+
+    public int[] keys;
+    Inventory playerInventory;
+    public bool[] aquiredKeys;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(int ID in keys)
+        {
+            if (playerInventory.inventoryIDs.Contains(ID))
+            {
+                aquiredKeys[System.Array.IndexOf(keys,ID)] = true;
+            }
+        }
+
+        if(Unlocked() == true)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+    public bool Unlocked()
+    {
+        for (int i = 0; i < aquiredKeys.Length; ++i)
+        {
+            if (aquiredKeys[i] == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
