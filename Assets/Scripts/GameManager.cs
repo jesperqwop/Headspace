593fe36d064d05f5b80125d1edafe93f15ff8007
Whale <mitr1659@edu.kadk.dk>
2020-04-02 09:46:28 +0200

    Testing water sounds

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class GameManager : MonoBehaviour
 {
+    public enum WaterStatus { Dry, Water, Underwater };
+    public static WaterStatus waterStatus = WaterStatus.Dry;
+
     public float waterRiseSpeed = 100;
 
     public float minimumDissolveRange;
@@ -15,35 +19,40 @@ public class GameManager : MonoBehaviour
     [Range(0, 1)]
     public float camSlider = 0;
     Scissor scissor2;
-
-    public Transform waterPlane;
-    public Transform plane2;
-
     public Transform apoint;
-    public float nearPlane;
-    public Vector3 thing;
 
     float x, y, z;
+
+    FirstPersonController controller;
+
+    AudioClip[] dryFootsteps;
+    public AudioClip[] wetFootsteps;
+  //  public firrs
+
     // Start is called before the first frame update
     void Start()
     {
+        controller = GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>();
+        dryFootsteps = controller.m_FootstepSounds;
         scissor2 = waterCam.GetComponent<Scissor>();
 
         z = mainCam.nearClipPlane;
         x = mainCam.fieldOfView / 2 / 100;
         y = x / (Screen.currentResolution.width / Screen.currentResolution.height);
+        UpdateSounds();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (apoint.position.y > (mainCam.transform.position.y + y))
-        { camSlider = 1;
-
+        {
+            camSlider = 1;
+            waterStatus = WaterStatus.Underwater;
         }
         else
         {
-            camSlider = mainCam.WorldToViewportPoint(new Vector3(mainCam.transform.position.x, apoint.position.y, mainCam.transform.position.z) + new Vector3(mainCam.transform.forward.x, 0, mainCam.transform.forward.z).normalized * nearPlane).y;
+            camSlider = mainCam.WorldToViewportPoint(new Vector3(mainCam.transform.position.x, apoint.position.y, mainCam.transform.position.z) + new Vector3(mainCam.transform.forward.x, 0, mainCam.transform.forward.z).normalized * mainCam.nearClipPlane).y;
         }
 
         camSlider = Mathf.Clamp(camSlider, 0, 1);
@@ -58,8 +67,8 @@ public class GameManager : MonoBehaviour
             waterCam.enabled = true;
 
             //   scissor2.scissorRect.y = 1 - camSlider;
-       //  Mathf.Clamp(camSlider)
-          scissor2.scissorRect.height = camSlider;
+            //  Mathf.Clamp(camSlider)
+            scissor2.scissorRect.height = camSlider;
         }
         else if (camSlider <= 0)
         {
@@ -67,5 +76,10 @@ public class GameManager : MonoBehaviour
         }
     }
 
-   
+    void UpdateSounds() {
+      //  if (waterStatus == WaterStatus.Underwater)
+        {
+            controller.m_FootstepSounds = wetFootsteps;
+        }
+    }
 }
---
+++ Assets/Scripts/Michael/WaterSource.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSource : MonoBehaviour
+{
+    AudioSource AS;
+    GameManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AS = GetComponentInChildren<AudioSource>();
+        manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.waterStatus == GameManager.WaterStatus.Underwater)
+        {
+            AS.spatialBlend = 1;
+        }
+        else
+        {
+            AS.spatialBlend = 0;
+        }
+    }
+}
