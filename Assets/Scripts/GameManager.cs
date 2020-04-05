66104cf20f227991e29c92d9606614234c3dcc99
Whale <mitr1659@edu.kadk.dk>
2020-04-06 00:32:45 +0200

    poosh

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -51,7 +51,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (apoint.position.y > (mainCam.transform.position.y + y))
+        if (apoint.position.y > (mainCam.transform.position.y + y) && waterStatus != WaterStatus.Underwater)
         {
             camSlider = 1;
             print("Underwater boys");
